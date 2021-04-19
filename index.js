b2d5fc309d10ab721ba300fc651e2a9a23e2fb26
NotLe0n <mr.sheep75@gmail.com>
2021-04-19 15:14:15 +0200

    dont work

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // setup
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
 const fetch = require('node-fetch');
 const app = express();
+
+// functions
 
 async function getModHasIcon(modName) {
   const url = "https://mirror.sgkoi.dev/direct";
@@ -23,6 +24,11 @@ async function getModInfo(modName) {
     .then(d => d.json());
 }
 
+async function getDescription(modName) {
+  let allDescriptions = await fetch('http://javid.ddns.net/tModLoader/tools/querymodnamehomepagedescription.php', { method: "Get" }).then(r => r.json());
+  return allDescriptions.filter(x => x.name == modName)[0].description;
+}
+
 // fixes file paths
 app.use(express.static(__dirname));
 //enables json use
@@ -38,25 +44,40 @@ app.listen(3000, () => {
   console.log('server started');
 });
 
+// post requests
 let fetchedMod;
 app.post('/getDescription', async (request, response) => {
   console.log("getting description");
-  let allDescriptions = await fetch('http://javid.ddns.net/tModLoader/tools/querymodnamehomepagedescription.php', { method: "Get" }).then(r => r.json());
-  response.status(200).json(allDescriptions.filter(x => x.name == fetchedMod)[0].description);
+  let description = await getDescription(fetchedMod);
+  response.status(200).json(description);
 });
 
-app.post('/idk', async (request, response) => {
-  var con = mysql.createConnection({
-    host: "sql7.freesqldatabase.com",
-    user: "sql7384742",
-    password: "GYR1aYu68T",
-    database: "sql7384742"
-  });
-  con.connect((err)=> {
-    if (err) console.log(err);
-    console.log("recieved mod list");
-  });
-  
+// get data from database and send it to front-end
+app.post('/getList', async (request, response) => {
+  const cheerio = require('cheerio');
+  const puppeteer = require('puppeteer');
+
+  const url = 'http://javid.ddns.net/tModLoader/modmigrationprogressalltime.php';
+  const mods = [];
+
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage();
+
+    const html = await page.goto(url).then(function () {
+      return page.content();
+    });
+
+    const $ = cheerio.load(html);
+    $('tr').each(tr => tr.each((td) => console.log(td.html())));
+  }
+  catch {
+    console.error();
+  }
+  response.status(200).json(mods);
+});
+
+app.post('/getModNames', async (request, response) => {
   let sql = `SELECT displayname FROM mods`;
 
   con.query(sql, async (err, result) => {
@@ -78,49 +99,34 @@ app.post('/idk', async (request, response) => {
 
 // get data from database and send it to front-end
 app.post('/api', async (request, response) => {
-  var con = mysql.createConnection({
-    host: "sql7.freesqldatabase.com",
-    user: "sql7384742",
-    password: "GYR1aYu68T",
-    database: "sql7384742"
-  });
-  con.connect((err)=> {
-    if (err) console.log(err);
-    console.log("connected to DB");
-  });
-  
-  let modname = con.escape(request.body.str);
-
+  let modname = request.body.str;
   console.log('Got a request: ' + modname);
-  let sql = `SELECT * FROM mods WHERE name=${modname} OR displayname=${modname}`;
+  
 
-  con.query(sql, async (err, result) => {
-    try {
-      const modInfo = getModInfo(result[0].name);
-      const pageUrl = getModPage(result[0].name);
-      const hasIcon = getModHasIcon(result[0].name);
+  try {
+    const modInfo = getModInfo(result[0].name);
+    const pageUrl = getModPage(result[0].name);
+    const hasIcon = getModHasIcon(result[0].name);
 
-      const data = await Promise.all([modInfo, pageUrl, hasIcon])
-        .then(([info, page, icon]) => ({
-          ...result[0],
-          ...info,
-          homepage: page || "no homepage",
-          hasIcon: icon,
-        }));
+    const data = await Promise.all([modInfo, pageUrl, hasIcon])
+      .then(([info, page, icon]) => ({
+        ...result[0],
+        ...info,
+        homepage: page || "no homepage",
+        hasIcon: icon,
+      }));
 
-      response.status(200).json(data);
-      fetchedMod = data.name; 
-    }
-    catch (err) {
-      console.error(err);
-      response.status(500);
-      response.json(null);
-    }
-  });
-  con.end();
+    response.status(200).json(data);
+    fetchedMod = data.name;
+  }
+  catch (err) {
+    console.error(err);
+    response.status(500);
+    response.json(null);
+  }
 });
 
 //stuff to do on exit
-process.on('exit', function() {
+process.on('exit', function () {
   console.log('About to close');
 });
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", async function() {
+document.addEventListener("DOMContentLoaded", async function () {
   // get all mod names from the database
-  var response = await fetch('/idk', { method: "POST" });
+  var response = await fetch('/getModNames', { method: "POST" });
   let modData = await response.json();
   for (let element in modData) {
-    let option = document.createElement("option");
-    option.innerHTML = modData[element].displayname;
-    document.getElementById("modlist").appendChild(option);
+      let option = document.createElement("option");
+      option.innerHTML = modData[element].displayname;
+      document.getElementById("modlist").appendChild(option);
   }
 
   document.getElementById("mod-search").setAttribute("list", "modlist");
@@ -15,9 +15,26 @@ document.addEventListener("DOMContentLoaded", async function() {
 function search(element) {
   var opts = document.getElementById('modlist').childNodes;
   for (var i = 0; i < opts.length; i++) {
-    if (opts[i].value === element.value) {
-      window.location.href = "/stats.html?mod=" + opts[i].value;
-      break;
-    }
+      if (opts[i].value === element.value) {
+          window.location.href = "/stats.html?mod=" + opts[i].value;
+          break;
+      }
   }
+}
+
+// sidebar stuff
+function closemenu() {
+  document.getElementById("Sidebar").style.width = "0";
+  document.body.style.marginLeft = "0";
+}
+
+function openmenu() {
+  document.getElementById("Sidebar").style.width = "250px";
+  document.body.style.marginLeft = "250px";
+}
+
+function getRandomMod() {
+  let mods = document.getElementById("modlist").options;
+  let rand = Math.floor(Math.random() * Math.floor(mods.length));
+  window.location.href = "/stats.html?mod=" + mods[rand].innerHTML;
 }
