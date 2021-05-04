af23a5bedb87551f245fb8c7e26edf83f2cf4ab9
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-05-04 08:27:16 +0200

    smooth nav animation

--- static/js/script.js
+++ static/js/script.js
@@ -38,10 +38,12 @@ async function randomMod() {
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("header").querySelector("h1").style.marginLeft = "210px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("header").querySelector("h1").style.marginLeft = "20px";
 }
